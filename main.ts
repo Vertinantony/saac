7ea858c8fd833a1dd5a34d6e65ea14dd6b6e4713
Vertinantony <114155080+Vertinantony@users.noreply.github.com>
2022-09-22 08:49:13 -0300

    Atualizar 'pxt.json, main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showString("prepare-se")
+    basic.pause(randint(1000, 5000))
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+})
 basic.forever(function () {
 	
 })
