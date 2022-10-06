758a9fd7523f7b1053c1f4f09507e99cce89db9e
Vertinantony <114155080+Vertinantony@users.noreply.github.com>
2022-10-06 08:48:37 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -7,13 +7,44 @@ input.onPinPressed(TouchPin.P0, function () {
     basic.showLeds(`
         . . # . .
         . . # . .
-        # # # # #
+        . . # . .
         # # # # #
         . # # # .
         `)
     basic.pause(1000)
     basic.clearScreen()
 })
+input.onPinPressed(TouchPin.P2, function () {
+    if (tocar == 0) {
+        cont2 += 1
+        basic.showLeds(`
+            . . . # #
+            . . . # #
+            . . . # #
+            . . . # #
+            . . . # #
+            `)
+        basic.pause(1000)
+        basic.clearScreen()
+    }
+})
+input.onPinPressed(TouchPin.P1, function () {
+    if (tocar == 0) {
+        cont1 += 1
+        basic.showLeds(`
+            # # . . .
+            # # . . .
+            # # . . .
+            # # . . .
+            # # . . .
+            `)
+        basic.pause(1000)
+        basic.clearScreen()
+    }
+})
+let tocar = 0
+let cont1 = 0
+let cont2 = 0
 basic.forever(function () {
 	
 })
